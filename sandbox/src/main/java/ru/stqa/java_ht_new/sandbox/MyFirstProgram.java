2d13899ffaabed70fb4e75cf5f72649580985a96
igoretk <balalayka2002@mail.ru>
2016-10-24 18:47:06 +0300

    some functions were added

--- sandbox/src/main/java/ru/stqa/java_ht_new/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_ht_new/sandbox/MyFirstProgram.java
@@ -2,6 +2,28 @@ package ru.stqa.java_ht_new.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("hello world");
+      hello("world");
+      hello("user");
+      hello("Igor");
+
+      double l = 5;
+      double a = 6;
+      double b = 8;
+      System.out.println("Площадь квадрата со стороной " + l + " равна " + areaSquare(l));
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + areaRectangle(a, b));
+
     }
+    public static void hello(String somebody) {
+
+      System.out.println("hello " + somebody);
+    }
+
+    public static double areaSquare(double l) {
+      return l * l;
+    }
+
+    public static double areaRectangle(double a, double b) {
+      return a * b;
+    }
+
 }
