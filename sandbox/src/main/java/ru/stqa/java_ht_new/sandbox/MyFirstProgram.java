4312ff59f9c42a93585100739023d8e37d01cb90
igoretk <balalayka2002@mail.ru>
2016-10-24 20:29:12 +0300

    some classes were added

--- sandbox/src/main/java/ru/stqa/java_ht_new/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_ht_new/sandbox/MyFirstProgram.java
@@ -1,29 +1,21 @@
 package ru.stqa.java_ht_new.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-      hello("world");
-      hello("user");
-      hello("Igor");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Igor");
+    Square s = new Square(5);
+    Rectangle r = new Rectangle(6, 8);
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.areaSquare());
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.areaRectangle());
 
-      double l = 5;
-      double a = 6;
-      double b = 8;
-      System.out.println("Площадь квадрата со стороной " + l + " равна " + areaSquare(l));
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + areaRectangle(a, b));
+  }
 
-    }
-    public static void hello(String somebody) {
+  public static void hello(String somebody) {
 
-      System.out.println("hello " + somebody);
-    }
+    System.out.println("hello " + somebody);
+  }
 
-    public static double areaSquare(double l) {
-      return l * l;
-    }
-
-    public static double areaRectangle(double a, double b) {
-      return a * b;
-    }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/java_ht_new/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.java_ht_new.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double areaSquare() {
+    return this.l * this.l;
+  }
+}
