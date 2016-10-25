de4c11962cabcc9b6e89d81e7625f811537abab8
igoretk <balalayka2002@mail.ru>
2016-10-25 18:03:13 +0300

    Revert "Вычисление расстояния функцией distance ркализовано в классе Paint"
    This reverts commit 28d91a9329fc6b673c119d5027167599c58c3b45.

--- sandbox/src/main/java/home/task02/sandbox/Executable.java
+++ sandbox/src/main/java/home/task02/sandbox/Executable.java
@@ -3,11 +3,22 @@ package home.task02.sandbox;
 public class Executable {
   public static void main(String[] args) {
 
-    Point point = new Point(3, 5, 2, 8);
-    double result = point.distance();
+    Point pointA = new Point();
+    Point pointB = new Point();
+    pointA.x1 = 3;
+    pointA.x2 = 5;
+    pointB.y1 = 2;
+    pointB.y2 = 3;
+    double result = distance(pointA, pointB);
     System.out.println("Расстояние между двумя точками A с координатами x1="
-            + point.x1 + " и x2=" + point.x2 +
-            " и B c координатами y1=" + point.y1 + " и y2=" + point.y2 +
+            + pointA.x1 + " и x2=" + pointA.x2 +
+            " и B c координатами y1=" + pointB.y1 + " и y2=" + pointB.y2 +
             " на плоскости равно: " + result);
+  }
+
+  public static double distance(Point pointA, Point pointB) {
+    double temp1 = (pointA.x2 - pointA.x1) * (pointA.x2 - pointA.x1); // для хранения части формулы (x2 - x1)^2
+    double temp2 = (pointB.y2 - pointB.y1) * (pointB.y2 - pointB.y1); // для хранения части формулы (y2 - y1)^2
+    return Math.sqrt(temp1 + temp2);
   }
 }
