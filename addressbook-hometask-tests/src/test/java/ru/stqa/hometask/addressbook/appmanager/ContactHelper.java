8ac47c89535fe221f6f4df1d66147346ce7d61d6
igoretk <balalayka2002@mail.ru>
2016-10-31 19:02:12 +0200

    helper base class was created

--- addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/appmanager/ContactHelper.java
+++ addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/appmanager/ContactHelper.java
@@ -6,67 +6,41 @@ import ru.stqa.hometask.addressbook.model.PhoneInfoData;
 import ru.stqa.hometask.addressbook.model.PrimaryInfoData;
 import ru.stqa.hometask.addressbook.model.SecondaryInfoData;
 
-public class ContactHelper {
-  private WebDriver wd;
+public class ContactHelper extends HelperBase {
 
   public ContactHelper(WebDriver wd) {
 
-    this.wd = wd;
+    super(wd);
   }
 
   public void submitNewContactCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void secondaryInfoFill(SecondaryInfoData secondaryInfoData) {
-    wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(secondaryInfoData.getSecondaryAddress());
-    wd.findElement(By.name("notes")).click();
-    wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys(secondaryInfoData.getNotes());
+    type(By.name("address2"), secondaryInfoData.getSecondaryAddress());
+    type(By.name("notes"), secondaryInfoData.getNotes());
   }
 
   public void phoneInfoFill(PhoneInfoData phoneInfoData) {
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(phoneInfoData.getHomePhone());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(phoneInfoData.getMobilePhone());
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(phoneInfoData.getWorkPhone());
-    wd.findElement(By.name("fax")).click();
-    wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys(phoneInfoData.getFax());
+    type(By.name("home"), phoneInfoData.getHomePhone());
+    type(By.name("mobile"), phoneInfoData.getMobilePhone());
+    type(By.name("work"), phoneInfoData.getWorkPhone());
+    type(By.name("fax"), phoneInfoData.getFax());
   }
 
   public void primaryInfoFill(PrimaryInfoData primaryInfoData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(primaryInfoData.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(primaryInfoData.getMiddleName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(primaryInfoData.getLastName());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(primaryInfoData.getNickName());
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(primaryInfoData.getTitle());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(primaryInfoData.getCompany());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(primaryInfoData.getAddress());
+    type(By.name("firstname"), primaryInfoData.getFirstName());
+    type(By.name("middlename"), primaryInfoData.getMiddleName());
+    type(By.name("lastname"), primaryInfoData.getLastName());
+    type(By.name("nickname"), primaryInfoData.getNickName());
+    type(By.name("title"), primaryInfoData.getTitle());
+    type(By.name("company"), primaryInfoData.getCompany());
+    type(By.name("address"), primaryInfoData.getAddress());
+
   }
 
   public void initNewContactCreation() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
--- addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/appmanager/GroupHelper.java
+++ addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/appmanager/GroupHelper.java
@@ -4,35 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.hometask.addressbook.model.DataGroupFilling;
 
-public class GroupHelper {
-  private WebDriver wd;
+public class GroupHelper extends  HelperBase {
 
   public GroupHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void goBackToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
+    click(By.linkText("group page"));
   }
 
   public void submitNewGroupCreation() {
-    wd.findElement(By.name("submit")).click();
+    click(By.name("submit"));
   }
 
   public void initNewGroupCreation() {
-    wd.findElement(By.name("new")).click();
+    click(By.name("new"));
   }
 
   public void fillingTheForm(DataGroupFilling dataGroupFilling) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(dataGroupFilling.getGroupName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(dataGroupFilling.getGroupHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(
-            dataGroupFilling.getGroupFooter());
+    type(By.name("group_name"), dataGroupFilling.getGroupName());
+    type(By.name("group_header"), dataGroupFilling.getGroupHeader());
+    type(By.name("group_footer"), dataGroupFilling.getGroupFooter());
   }
+
 }
