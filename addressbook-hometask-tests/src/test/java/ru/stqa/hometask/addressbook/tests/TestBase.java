823a4a6172da420b0e7f5e18f9600a495fb150f3
igoretk <balalayka2002@mail.ru>
2016-11-13 22:36:44 +0200

    запускаем браузер один раз для всех тестов
    1. изменены аннотации @BefreTest и @AfterTest на @BeforeSuite и
    @AfterSuite
    2. переменная applicationManager объявлена как static

--- addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/tests/TestBase.java
+++ addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/tests/TestBase.java
@@ -2,20 +2,22 @@ package ru.stqa.hometask.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.hometask.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager applicationManager = new ApplicationManager(BrowserType.FIREFOX);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     applicationManager.init();
 
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     applicationManager.stop();
   }
