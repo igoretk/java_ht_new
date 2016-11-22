79f2eb0d87f669b50520686b8b0a49a0ffad047b
igoretk <balalayka2002@mail.ru>
2016-11-22 19:17:20 +0200

    тест по созданию групп, запись чтение тестовых данных xml

--- addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/tests/GroupTestCreation.java
+++ addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/tests/GroupTestCreation.java
@@ -1,5 +1,6 @@
 package ru.stqa.hometask.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -93,6 +95,34 @@ public class GroupTestCreation extends TestBase {
 
   @Test(dataProvider = "validGroupsFromFile")
   public void testGroupCreationWithDataProviderFromFile(DataGroupFilling group) {
+    app.goTo().GroupPage();
+    Groups before = app.group().all();
+    app.group().create(group);
+    Groups after = app.group().all();
+    assertThat(after.size(), equalTo(before.size() + 1));
+
+    assertThat(after, equalTo(
+            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromFileXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(DataGroupFilling.class);
+    List<DataGroupFilling> groups = (List<DataGroupFilling>) xStream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromFileXml")
+  public void testGroupCreationWithFromFileXml(DataGroupFilling group) {
     app.goTo().GroupPage();
     Groups before = app.group().all();
     app.group().create(group);
