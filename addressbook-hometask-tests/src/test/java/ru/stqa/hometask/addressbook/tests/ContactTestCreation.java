abdbb9ba6f36fbcb4b27ab56bab1257bbcfa032c
igoretk <balalayka2002@mail.ru>
2016-11-22 20:30:32 +0200

    реализован тест по созданию группы с датапровайдером чтения из json

--- addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/tests/ContactTestCreation.java
+++ addressbook-hometask-tests/src/test/java/ru/stqa/hometask/addressbook/tests/ContactTestCreation.java
@@ -1,13 +1,24 @@
 package ru.stqa.hometask.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.hometask.addressbook.model.Contacts;
 import ru.stqa.hometask.addressbook.model.DataContactFilling;
+import ru.stqa.hometask.addressbook.model.DataGroupFilling;
+import ru.stqa.hometask.addressbook.model.Groups;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -157,4 +168,32 @@ public class ContactTestCreation extends TestBase {
     System.out.println(photo.exists());
 
   }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromFileJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<DataContactFilling> contacts = gson.fromJson(json, new TypeToken<List<DataContactFilling>>(){}.getType()); // List<DataContactFilling>.class
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromFileJson")
+  public void testContactCreationWithFromFileJson(DataContactFilling contact) {
+    app.goTo().ContactPage();
+    Contacts before = app.contact().all();
+    app.contact().create(contact);
+    Contacts after = app.contact().all();
+    assertThat(after.size(), equalTo(before.size() + 1));
+
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
+  }
+
 }
