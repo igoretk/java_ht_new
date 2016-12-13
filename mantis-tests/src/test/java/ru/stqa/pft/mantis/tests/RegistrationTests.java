a2bbbb17ade9816e1181c660ee6ad67c1ae312f5
igoretk <balalayka2002@mail.ru>
2016-12-13 20:01:17 +0200

    testRegistration edit

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -25,9 +25,9 @@ public class RegistrationTests extends TestBase {
   @Test
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost.localdomain", now);
     String user = String.format("user%s", now);
     String password = "password";
+    String email = String.format("user%s@localhost.localdomain", now);
     app.james().createUser(user, password);
 
     app.registration().start(user, email);
